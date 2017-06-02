fb7ecbf31e79b239f1b0d2bad0b1825e7b945064
rrrmhhh <rrrmhhh@gmail.com>
2017-06-02 17:33:42 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './App'
+// import registerServiceWorker from './registerServiceWorker'
+// import './index.css'
+
+ReactDOM.render(<div>
+  <h1>Hello World!</h1>
+</div>, document.getElementById('root'))
+// registerServiceWorker()
